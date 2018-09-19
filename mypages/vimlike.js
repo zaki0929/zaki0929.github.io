6216293a68848011695ac0b9786a1a8714d0b518
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2018-09-20 00:32:17 +0900

    Delete previous page because migration is over

--- mypages/vimlike.js
+++
@@ -1,6 +0,0 @@
-document.onkeydown = function(event) {
-    if(event.keyCode === 72) window.scrollBy(-20, 0);
-    if(event.keyCode === 74) window.scrollBy(0, 20);
-    if(event.keyCode === 75) window.scrollBy(0, -20);
-    if(event.keyCode === 76) window.scrollBy(20, 0);
-}
