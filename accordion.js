ac78236fdc74a7f506c1c229440939e26b728048
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2020-04-24 09:27:32 +0900

    Update style

--- accordion.js
+++ accordion.js
@@ -2,8 +2,8 @@ window.addEventListener("load", function(){
   var menu = document.getElementsByClassName('accordion');
   
   function show(){
+    this.classList.toggle('push');
     this.nextElementSibling.classList.toggle('hide');
-    this.childNodes[1].classList.toggle('active');
   };
   
   for (var i=0; i<menu.length; i++){
